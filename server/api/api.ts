f0c3c53f3bcf164047dc1c351f1f99853d059eda
Matheus351 <matheusniklaus@gmail.com>
2023-02-24 16:20:18 -0300

    adicionando errorHandler da api

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import { errorHandlerApi } from './errorHandlerApi';
 
 import Routes from './routes/routes';
 
@@ -17,6 +18,7 @@ class Api {
         this.express.use(morgan('dev'));
         this.express.use(bodyParser.urlencoded({ extended: true }));
         this.express.use(bodyParser.json());
+        this.express.use(errorHandlerApi);
     }
 
     private router(express: Application): void {
